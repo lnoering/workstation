4e75d349fc7982acbbde6da158932d862257d9b9
root <root@localhost.localdomain>
2018-06-09 22:08:46 -0300

    fix - intall vlc.

--- requireds/fedora/players.sh
+++ requireds/fedora/players.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-sudo dnf install vlc
+
+sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm -y
+
+sudo dnf install vlc -y
 
 #qmmp
