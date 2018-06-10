717c3e174baf937063e9b17a8a036c2b4a47c49e
root <root@localhost.localdomain>
2018-06-09 22:01:12 -0400

    fix - install workbench

--- requireds/fedora/workbench.sh
+++ requireds/fedora/workbench.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 #irá jogar uma mensagem na tela de alerta pq não é para a versão 28.
-wget http://dev.mysql.com/get/Downloads/MySQLGUITools/mysql-workbench-community-6.3.10-1.fc27.x86_64.rpm
+sudo wget http://dev.mysql.com/get/Downloads/MySQLGUITools/mysql-workbench-community-6.3.10-1.fc27.x86_64.rpm
 
 sudo dnf -y install ./mysql-workbench-community-6.3.10-1.fc27.x86_64.rpm
 
-rm ./mysql-workbench-community-6.3.10-1.fc27.x86_64.rpm
+sudo rm ./mysql-workbench-community-6.3.10-1.fc27.x86_64.rpm
 #sudo dnf -y install mysql-workbench-community
